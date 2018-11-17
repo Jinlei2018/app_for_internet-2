18be87646c1b86b66a15051ca4bae63ca596bbc3
Jinlei2018 <43339636+Jinlei2018@users.noreply.github.com>
2018-11-17 22:56:50 +0100

    Add files via upload

---
+++ components/head.php
@@ -0,0 +1,12 @@
+<?php
+function write_header($title)
+{
+    ?>
+		<link href = "./reset.css" rel = "stylesheet" type = "text/css"/>
+		<link href = "./layout.css" rel = "stylesheet" type = "text/css"/>
+		<link href = "./custom.css" rel = "stylesheet" type = "text/css"/>
+		<title>Tasty Recipes-Home</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
+}
